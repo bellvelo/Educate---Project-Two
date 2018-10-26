710c236a4c79fa4a71af6896fe993cf5c272cfd3
David Bell <dbelldev@icloud.com>
2018-10-26 14:37:36 +0100

    set up

---
+++ server/db/seeds.js
@@ -0,0 +1,58 @@
+use bbc;
+
+db.dropDatabase();
+
+db.global.insertMany([
+  {
+    title: "problem",
+    intro: "problem-intro",
+    carousel: {
+      carousel_one {
+        image: "url",
+        text: "text1"
+      },
+      carousel_two {
+        image: "url",
+        text: "text1"
+      },
+      carousel_three {
+        image: "url",
+        text: "text1"
+      }
+    },
+    question: {
+      question_text: "problem-question-text",
+      correct_answer: "correct",
+      incorrect_answers: [
+        "wrong1",
+        "wrong2",
+        "wrong3"
+      ]
+
+    },
+    {
+      title: "solution",
+      intro: "solution-intro",
+      carousel: {
+        carousel_one {
+          image: "url",
+          text: "text1"
+        },
+        carousel_two {
+          image: "url",
+          text: "text1"
+        },
+        carousel_three {
+          image: "url",
+          text: "text1"
+        },
+        question: {
+          question_text: "problem-question-text",
+          correct_answer: "correct",
+          incorrect_answers: [
+            "wrong1",
+            "wrong2",
+            "wrong3"
+          ]
+        }
+      ]);
