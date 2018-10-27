bcdc7e2f38df6788a0f08d71cb23df87779791e1
David Bell <dbelldev@icloud.com>
2018-10-27 16:34:30 +0100

    view added

---
+++ client/src/views/question_detail_view.js
@@ -0,0 +1,12 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const QuestionDetailView = function(container){
+  this.container = container;
+};
+
+QuestionDetailView.prototype.render = function () {
+
+};
+
+
+module.exports = QuestionDetailView;
