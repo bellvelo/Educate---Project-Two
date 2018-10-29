796599ff71b497121ce947ab5daacdc5aefa5782
Neil Ruthven <Nruthven@yahoo.com>
2018-10-29 10:12:05 +0000

    clean merge

--- client/src/views/question_view.js
+++ client/src/views/question_view.js
@@ -10,17 +10,6 @@ QuestionView.prototype.render = function (topic) {
   const container = document.createElement('div');
   container.classList.add('question-container');
 
-<<<<<<< HEAD
-  const question = document.createElement('h4');
-  question.classList.add('question');
-  question.textContent = topic.question.question_text;
-  container.appendChild(question);
-
-  const optionContainer = document.createElement('div');
-  optionContainer.classList.add('options');
-  this.renderOptions(topic, container);
-
-=======
   const question = document.createElement('div');
   question.classList.add('question');
 
@@ -34,23 +23,17 @@ QuestionView.prototype.render = function (topic) {
   this.renderOptions(topic, question);
 
   container.appendChild(question)
->>>>>>> master
+
   this.container.appendChild(container);
 };
 
 QuestionView.prototype.renderOptions = function (topic, container) {
 
-<<<<<<< HEAD
-  topic.question.answers.forEach((answer) => {
-    const button = document.createElement('button');
-    button.classList.add('topic-button');
-=======
 
   topic.question.answers.forEach((answer) => {
     const button = document.createElement('button');
-    button.classList.add('question-button');
-    button.textContent = answer;
->>>>>>> master
+    button.classList.add('topic-button');
+
     button.value = answer;
     container.appendChild(button);
 
--- server/db/seeds.js
+++ server/db/seeds.js
@@ -6,6 +6,12 @@ db.global.insertMany([
   {
     title: "The effects of global warming",
     intro: "Although the temperature increases have been gradual, most, but not all, scientists agree that the climate is getting gradually warmer. This is called global warming. Most, but not all, scientists lay the blame for this on human activities increasing the amount of carbon dioxide in the atmosphere. Global warming could cause climate change and extreme weather conditions in some areas.",
+    info_points: [
+      "What is global warming",
+      "The main causes of global warming",
+      "What effect does it have on you ",
+      "Test your knowledge"
+    ],
     carousel: {
       carousel_one: {
         image: "url",
@@ -33,35 +39,40 @@ db.global.insertMany([
     }
   },
 
-    {
-      title: "Renewable Energy - the Solution?",
-      intro: "Renewable energy is sustainable and will never run out. They provide clean energy because they are non-pollutant and non-contributor to greenhouse effects and global warming. Renewable energy facilities generally require less maintenance than traditional generators. Their fuel being derived from natural and available resources reduces the costs of operation.",
-      carousel: {
-        carousel_one: {
-          image: "url",
-          text: "text1"
-        },
-        carousel_two: {
-          image: "url",
-          text: "text1"
-        },
-        carousel_three: {
-          image: "url",
-          text: "text1"
-        },
+  {
+    title: "Renewable Energy - the Solution?",
+    intro: "Renewable energy is sustainable and will never run out. They provide clean energy because they are non-pollutant and non-contributor to greenhouse effects and global warming. Renewable energy facilities generally require less maintenance than traditional generators. Their fuel being derived from natural and available resources reduces the costs of operation.",
+    info_points: [
+      "Is renewable energy the future? ",
+      "How we can all help",
+      "Learn from the past",
+      "Calculate your annual carbon footprint"
+    ],
+    carousel: {
+      carousel_one: {
+        image: "url",
+        text: "text1"
       },
-        question: {
-          question_text: "Which Region is the largest investor in renewable energies? (source: ourworldindata.org)",
-          correct_answer: "China",
-          answers: [
-            "Europe",
-            "United States of America",
-            "China",
-            "Asia & Oceania"
-          ]
-        }
-
+      carousel_two: {
+        image: "url",
+        text: "text1"
+      },
+      carousel_three: {
+        image: "url",
+        text: "text1"
+      },
+    },
+    question: {
+      question_text: "Which Region is the largest investor in renewable energies? (source: ourworldindata.org)",
+      correct_answer: "China",
+      answers: [
+        "Europe",
+        "United States of America",
+        "China",
+        "Asia & Oceania"
+      ]
     }
 
   }
+
 ]);
